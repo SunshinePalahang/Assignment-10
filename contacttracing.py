7a6ff5cfd774bd53a9d39b2251892d5c95c97eab
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2022-02-15 12:29:38 +0800

    start new with import decode

--- contacttracing.py
+++ contacttracing.py
@@ -1,20 +1,6 @@
 import cv2
 import numpy as np
-import pyzbar.pyzbar as pyzbar
+from pyzbar.pyzbar import decode
+import datetime
 
-#start the cam
-cam = cv2.VideoCapture(0)
-#detect qrcode
-detect = cv2.QRCodeDetector
 
-while True:
-    _, img = cam.read()
-
-    detect = pyzbar.decode(img)
-    for obj in detect:
-        print(obj.data)
-
-    cv2.imshow("Frame", img)
-    key = cv2.waitKey(1)
-    if key == 1:
-        break
